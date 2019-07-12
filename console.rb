b4dd42a7e5d3eb673b35df1359a0a427080b94cd
Fidelma <fidelma.beagan@gmail.com>
2019-07-12 13:34:08 +0100

    CRUD for film added

--- console.rb
+++ console.rb
@@ -1,7 +1,9 @@
 require('pry')
 require_relative('models/customer')
+require_relative('models/film')
 
 Customer.delete_all()
+Film.delete_all()
 
 
 customer1 = Customer.new({
@@ -9,7 +11,14 @@ customer1 = Customer.new({
   'funds' => 20
   })
 
-  # customer1.save()
+  customer1.save()
+
+  film1 = Film.new({
+    'title' => 'John Wick',
+    'price' => 7
+    })
+
+  film1.save
 
   binding.pry
   nil
